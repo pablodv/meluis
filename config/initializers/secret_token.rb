be577a0b972a4eb4cd76d910abcf2adc1b235ab0
Pablo <pablo.moreiramora@gmail.com>
2012-08-01 14:10:13 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Melius::Application.config.secret_token = 'a030cb04b3cbf9174c4e00b32fc868c4b2f7bb7d48cf23ae304a071ca1fea1b4fabfa116b422ea59558b44b21003d3839aac3005143685bd1048cbd3be9f9c5e'
